4a29c22027f4fa75efdd9adb10eb5bc88d1ae0f2
jackiehj <32741134+jackiehj@users.noreply.github.com>
2022-05-25 04:06:18 +0800

    Update lede.sh

--- script/lede.sh
+++ script/lede.sh
@@ -17,7 +17,7 @@ cp -f $GITHUB_WORKSPACE/pics/bg1.jpg luci-theme-argon/htdocs/luci-static/argon/i
 popd
 
 # Modify default IP
-sed -i 's/10.10.10.1/10.10.10.254/g' package/base-files/files/bin/config_generate
+sed -i 's/10.10.10.1/10.10.10.2/g' package/base-files/files/bin/config_generate
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='JackieWRT'' package/lean/default-settings/files/zzz-default-settings
 sed -i "s/OpenWrt /Jackie - Huang /g" package/lean/default-settings/files/zzz-default-settings
 # find package/*/ feeds/*/ -maxdepth 6 -path "*luci-app-smartdns/luasrc/controller/smartdns.lua" | xargs -i sed -i 's/\"SmartDNS\")\, 4/\"SmartDNS\")\, 3/g' {} 
@@ -25,7 +25,7 @@ sed -i "s/OpenWrt /Jackie - Huang /g" package/lean/default-settings/files/zzz-de
 # sed -i 's/5.10/5.15/g' target/linux/x86/Makefile
 
 # Network Configuration
-sed -i "/exit/iuci set network.lan.gateway=\'10.10.10.2\'\nuci set network.lan.dns=\'119.29.29.29 223.5.5.5\'\nuci commit network\nuci set dhcp.lan.ignore=\'1\'\nuci set dhcp.lan.dhcpv6=\'disabled\'\nuci commit dhcp\n" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit/iuci set network.lan.gateway=\'10.10.10.1\'\nuci set network.lan.dns=\'119.29.29.29 223.5.5.5\'\nuci commit network\nuci set dhcp.lan.ignore=\'1\'\nuci set dhcp.lan.dhcpv6=\'disabled\'\nuci commit dhcp\n" package/lean/default-settings/files/zzz-default-settings
         
 # nlbwmon netlink uffer size
 sed -i '1s/$/&\nnet.core.wmem_max=16777216\nnet.core.rmem_max=16777216/' package/base-files/files/etc/sysctl.conf
